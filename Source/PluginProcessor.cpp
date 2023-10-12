9c2c31c27c3d2744522360aec6b18c22ef7d7dd0
Isaac Vergara <iamiv097@gmail.com>
2023-10-11 23:59:35 -0500

    Fixes

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -131,7 +131,7 @@ void MultibandCompressorAudioProcessor::releaseResources()
 
 	APFR.setSR(FS);
 	APFR.setFilterType(3);
-	APFR.setFc(1);
+	APFR.setFc(4e3);
 	APFL.setSR(FS);
 	APFL.setFilterType(3);
 	APFL.setFc(4e3);
@@ -227,31 +227,6 @@ void MultibandCompressorAudioProcessor::processBlock(juce::AudioBuffer<float>& b
 
 
 
-		}
-	}
-
-	// In case we have more outputs than inputs, this code clears any output
-	// channels that didn't contain input data, (because these aren't
-	// guaranteed to be empty - they may contain garbage).
-	// This is here to avoid people getting screaming feedback
-	// when they first compile a plugin, but obviously you don't need to keep
-	// this code if your algorithm always overwrites all the output channels.
-	for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-		buffer.clear(i, 0, buffer.getNumSamples());
-
-	// This is the place where you'd normally do the guts of your plugin's
-	// audio processing...
-	// Make sure to reset the state if your inner loop is processing
-	// the samples and the outer loop is handling the channels.
-	// Alternatively, you can process the samples with the channels
-	// interleaved by keeping the same state.
-	for (int sample = 0; sample < buffer.getNumSamples(); ++sample)
-	{
-		for (int channel = 0; channel < totalNumInputChannels; ++channel)
-		{
-			auto* channelData = buffer.getWritePointer(channel);
-
-			// ..do something to the data...
 		}
 	}
 }
